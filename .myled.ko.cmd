05aad61d6e888d96393a4b8a1891eb53b7a3a2fe
ryousuke <S20C1044NA@s.chibakoudai.jp>
2021-12-15 22:09:08 +0000

    add a new file.

---
+++ .myled.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ubuntu/robosys1/myled.ko := ld -r  -EL  -maarch64elf  --build-id  -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds -o /home/ubuntu/robosys1/myled.ko /home/ubuntu/robosys1/myled.o /home/ubuntu/robosys1/myled.mod.o;  true
---
+++ .myled.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ubuntu/robosys1/myled.mod := { echo  /home/ubuntu/robosys1/myled.o;  echo; } > /home/ubuntu/robosys1/myled.mod
